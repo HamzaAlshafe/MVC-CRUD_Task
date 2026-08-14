884e7af2172b6011d76d9fc0bd9fa189f5debbe3
HamzaAlshafei <pshamza99@gmail.com>
2026-08-14 19:44:27 +0300

    initial commit

---
+++ Task/Task/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task.Models;
+
+namespace Task.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=.;Database=MVCCRUDTASK;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";Command Timeout=0");
+        }
+    }
+}
---
+++ Task/Task/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 char")]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 char")]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [MaxLength(250, ErrorMessage = "Description cannot exceed 250 char")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price Is Required")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Rate Is Required")]
+        [Range(1, 5, ErrorMessage = "Rate must be between 1 and 5")]
+        [Display(Name = "Rating")]
+        public int rate { get; set; }
+    }
+}
